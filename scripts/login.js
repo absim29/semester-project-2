3990a6599ee614ac89ff4ff260aeb1d90b4fd215
absim29 <simon.bianca29@gmail.com>
2024-02-10 16:07:09 +0100

    Login JS

---
+++ scripts/login.js
@@ -0,0 +1,34 @@
+import { LOGIN_API_URL } from "./constants.js";
+import { fetchData } from "./fetchData.js";
+import { displayListings } from "./utils/allListings.js";
+import { addToLocalStorage } from "./utils/localStorage.js";
+
+const form = document.querySelector('#loginForm');
+const email = document.querySelector('#login-email');
+const password = document.querySelector('#login-password');
+
+async function loginUser(user) {
+    try {
+        const postBody = JSON.stringify(user);
+        const userLoginData = await fetchData(LOGIN_API_URL, {
+            method: 'POST',
+            body: postBody,
+        }, false);
+        const token = userLoginData.accessToken;
+        addToLocalStorage('accessToken', token);
+        addToLocalStorage('userEmail', userLoginData.email);
+        window.location.href = '../listings';
+    } catch (error) {
+        document.querySelector('#error').innerHTML = '<div class="error">Wrong email or password</div>';
+    }
+}
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const userLoginDetails = {
+        email: email.value,
+        password: password.value,
+    };
+    await loginUser(userLoginDetails);
+    displayListings();
+})
