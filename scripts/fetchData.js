7b13004f16a98c3f3d397f6b81d9b7ae2eba3264
absim29 <simon.bianca29@gmail.com>
2024-02-10 16:04:50 +0100

    fetch the data js

---
+++ scripts/fetchData.js
@@ -0,0 +1,25 @@
+import { getFromLocalStorage } from "./utils/localStorage.js";
+
+export async function fetchData(url, options = { method: 'GET' }, shouldUseAuth = false,) {
+
+    let fetchOptions = {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    }
+    if (shouldUseAuth) {
+        const accessToken = getFromLocalStorage('accessToken');
+        fetchOptions = {
+            ...fetchOptions,
+            headers: { ...fetchOptions.headers, Authorization: `Bearer ${accessToken}` }
+        };
+    }
+    const response = await fetch(url, fetchOptions);
+    if (!response.ok) {
+        throw new Error('API call unsuccessful');
+    }
+    const data = await response.json();
+    return data;
+
+}
