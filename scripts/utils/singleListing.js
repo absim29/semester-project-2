8a7612c10edbef137551c23020553c74401dc365
absim29 <simon.bianca29@gmail.com>
2024-02-13 14:15:48 +0100

    Create singleListing.js

---
+++ scripts/utils/singleListing.js
@@ -0,0 +1,17 @@
+import { LISTINGS_API_URL } from "../constants.js";
+import { fetchData } from "../fetchData.js";
+
+function singlePostId() {
+    const postId = new URLSearchParams(window.location.search).get('id');
+    return postId;
+}
+
+async function getSinglePost(id) {
+    const singlePostApi = `${LISTINGS_API_URL}/${id}`;
+    const currentPost = await fetchData(singlePostApi, {
+        method: 'GET',
+    }, true);
+    return currentPost;
+}
+
+export { singlePostId, getSinglePost }
