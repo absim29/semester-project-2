c303f1b0243893d0aa9c38f13956480775fcd2f1
absim29 <simon.bianca29@gmail.com>
2024-02-14 22:46:30 +0100

    Delete JS

---
+++ scripts/deleteListing.js
@@ -0,0 +1,22 @@
+import { LISTINGS_API_URL } from "./constants.js";
+import { fetchData } from "./fetchData.js";
+import { singlePostId } from "./utils/singleListing.js";
+
+async function deletePost() {
+    try {
+
+        const postId = singlePostId();
+        const response = await fetchData(`${LISTINGS_API_URL}/${postId}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            },
+        }, true);
+        return { success: true, response }
+    }
+    catch (error) {
+        return { success: false, error }
+    }
+}
+
+export { deletePost };
