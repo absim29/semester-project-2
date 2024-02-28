bf207f18de5a2a2ae472293262c234d121b4aaeb
absim29 <simon.bianca29@gmail.com>
2024-02-28 23:17:22 +0100

    Update searchUtil.js

--- scripts/utils/searchUtil.js
+++ scripts/utils/searchUtil.js
@@ -1,15 +1,9 @@
-function filerPostHandler(post) {
+function filterPostHandler(post) {
 
     if (post.title.toLowerCase().match(searchInput.value.toLowerCase().trim())) {
         return true;
     }
     return false;
-    // searchTerm = String(searchTerm);
-
-    // if (post.title && searchTerm && post.title.toLowerCase().includes(searchTerm.toLowerCase().trim())) {
-    //     return true;
-    // }
-    // return false;
 }
 
-export { filerPostHandler };
+export { filterPostHandler };
