82c59a96c3340a8cfa2b152ca5284ccb902beaa3
absim29 <simon.bianca29@gmail.com>
2024-02-29 01:42:44 +0100

    Seller page JS

---
+++ scripts/sellerProfilePage.js
@@ -0,0 +1,55 @@
+import { PROFILES_API_URL } from "./constants.js";
+import { getFromLocalStorage } from "./utils/localStorage.js";
+import { userProfileHTML } from "./utils/profileHTML.js";
+import { generatePostHtml } from "./utils/generateHTML.js"
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const name = params.get("name");
+
+const sellerAPI = PROFILES_API_URL + `/` + name;
+
+async function sellerUser() {
+    const profileContainer = document.getElementById('profileContainer');
+    profileContainer.textContent = '';
+    try {
+
+        const accessToken = getFromLocalStorage('accessToken');
+        // Fetch user profile
+        const response = await fetch(sellerAPI, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${accessToken}` },
+        }, true);
+
+        const profileData = await response.json();
+        console.log(profileData);
+
+        // Fetch user listings
+        const listingsResponse = await fetch(sellerAPI + `/listings`, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${accessToken}` },
+        });
+
+        const listingsData = await listingsResponse.json();
+        console.log(listingsData);
+
+        // Render profile HTML
+        const profile = userProfileHTML(profileData);
+        profileContainer.appendChild(profile);
+
+        // Render listings HTML
+        const listingsContainer = document.getElementById('listingsContainer');
+        listingsContainer.innerHTML = ''; // Clear previous listings
+
+        listingsData.forEach(listing => {
+            const listingElement = generatePostHtml(listing);
+            listingsContainer.appendChild(listingElement);
+        });
+
+    } catch (error) {
+        console.error('Error fetching profile:', error);
+        profileContainer.innerHTML = '<p>Error fetching profile data.</p>';
+    }
+};
+
+sellerUser();
+
