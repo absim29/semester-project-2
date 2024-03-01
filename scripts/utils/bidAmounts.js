1389547bebfca0c066e82b4e8121a3b6dfb1d6cb
absim29 <simon.bianca29@gmail.com>
2024-03-02 00:06:32 +0100

    Update bidAmounts.js

--- scripts/utils/bidAmounts.js
+++ scripts/utils/bidAmounts.js
@@ -5,15 +5,21 @@ import { LISTINGS_API_URL } from "../constants.js";
 
 
 async function bidAmounts() {
-    const BID_API_URL = `${LISTINGS_API_URL}/${singlePostId()}/?_bids=true`;
-    const response = await fetchData(BID_API_URL, {
-        method: 'GET',
-    }, true)
-    const bids = response.bids;
-    bids.forEach(bid => {
-        const bidAmount = bid.amount;
-        console.log('Bid Amount:', bidAmount);
-    });
+    try {
+        const BID_API_URL = `${LISTINGS_API_URL}/${singlePostId()}/?_bids=true`;
+        const response = await fetchData(BID_API_URL, {
+            method: 'GET',
+        }, true)
+        const bids = response.bids;
+        bids.forEach(bid => {
+            const bidAmount = bid.amount;
+            console.log('Bid Amount:', bidAmount);
+        });
+        return bids;
+    } catch (error) {
+        console.error('Error fetching bid amounts:', error);
+        throw error;
+    }
 
 }
 
